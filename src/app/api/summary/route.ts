43e910483591e02f4f30f8c3609c7536890e49e5
Rohan Giri <giri.dev.rohan@gmail.com>
2026-06-22 02:25:17 +0530

    add upated route to get all summaray just from month

--- src/app/api/summary/route.ts
+++ src/app/api/summary/route.ts
@@ -1,9 +1,31 @@
 import { prisma } from "@/lib/prisma";
-import {  NextResponse } from "next/server";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(){
+export async function GET(req : NextRequest){
     try {
+        const {searchParams} = new URL(req.url)
+        const month = searchParams.get("month")
+
+        if(!month){
+            return NextResponse.json({error: "Month is required"}, {status: 400})
+        }
+
+        const [year, monthNumber] = month.split("-").map(Number);
+
+        // Create date range for the selected month:
+        // startDate = first day of selected month
+        // endDate = first day of next month
+        const startDate = new Date(Date.UTC(year, monthNumber - 1, 1));
+        const endDate = new Date(Date.UTC(year, monthNumber, 1));
+        
         const transactions = await prisma.transaction.findMany({
+            where: {
+                date: {
+                    gte: startDate, // greater then or eaual to >=
+                    lt: endDate, // less than <
+                }
+            },
             orderBy: {
                 date: "desc"
             }
@@ -11,7 +33,10 @@ export async function GET(){
 
         return NextResponse.json({transactions}, {status: 200});
     } catch (error) {
-        console.error("Failed to GET all the transactions", error)
-        return NextResponse.json({error: "Failed to GET all the transactions"}, {status: 400})
-    }
+    console.error("Failed to GET summary", error);
+    return NextResponse.json(
+      { error: "Failed to GET summary" },
+      { status: 500 }
+    );
+  }
 }
